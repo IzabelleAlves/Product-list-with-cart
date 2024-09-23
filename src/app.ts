617e780fab87f15ae40844bf7d3ececf3027b1aa
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-09-23 10:07:56 -0300

    Renderizando produtos

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,11 @@ import { Produto } from "./entites/product";
 import data from "../data.json" // data é variável. Não utiliza a chave porque aqui é um dado bruto
 
 for (let i = 0; i < data.length; i++){
-    const produto1 = new Produto(data[i].name, data[i].category, data[i].price, data[i].image.desktop)
-    console.log(produto1)
+    const produto = new Produto(
+        data[i].name, 
+        data[i].category, 
+        data[i].price, 
+        data[i].image.desktop
+    );
+    produto.render()
 }
-
--- src/entites/product.ts
+++ src/entites/product.ts
@@ -7,7 +7,6 @@ export class Produto {
     private _price: number;
     private _category: string;
 
-
     constructor (
         name: string, 
         category: string, 
@@ -18,5 +17,62 @@ export class Produto {
         this._category = category,
         this._price = price,
         this._imageUrl = imageUrl
+    }
+    
+    render() {
+        const containerPrincipal = document.createElement("div")
+        containerPrincipal.className = "container-principal"
+
+        const products = document.createElement("div")
+        products.className = "cards"    
+        products.innerHTML = `
+            <div class="product-card">
+                <img class= "product-image" src="${this._imageUrl}" alt="${this._name}">
+                <div class="product-information">
+                    <div class="product-category">${this._category}</div>
+                    <div class="product-title">${this._name}</div>
+                    <div class="product-price">$${this._price.toFixed(2)}</div>
+            </div>
+            </div>
+            <div class="add-cart-btn">
+                <div class="add-cart-icon">
+                    <i class="fa fa-cart-plus" aria-hidden="true"></i>
+                </div>
+                <span>Add to Cart</span>
+            </div>
+        `; 
+        
+
+        // const addToCart = document.createElement("div")
+        // addToCart.innerHTML = `
+        // <div class="add-cart-btn">
+        //         <div class="add-cart-icon">
+        //             <i class="fa fa-cart-plus" aria-hidden="true"></i>
+        //         </div>
+        //         <span>Add to Cart</span>
+        //     </div>
+        //     `;
+
+        // const cart = document.createElement("div")
+        // cart.className = "cart"
+        // cart.innerHTML = `
+        //     <div class="header-cart">Your Cart</div>
+        // `;
+
+        containerPrincipal.append(products)
+
+        const mainContainer = document.getElementById('main-id')
+        if (mainContainer) {
+            mainContainer.appendChild(containerPrincipal)
+        }    
+        return containerPrincipal;
+    }
+    
+    add() {
+        return this._id;
+    }
+    
+    remove() {
+        console.log("olá")
     }
 }
