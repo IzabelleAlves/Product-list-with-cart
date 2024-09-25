b240389a5c91e66b3cd20aae8262a4be3b8cc425
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-09-25 15:24:45 -0300

    Atualização no modal

--- src/entites/cart.ts
+++ src/entites/cart.ts
@@ -65,12 +65,12 @@ export class Cart {
                 itemElement.innerHTML = `
                     <div class="infoName">
                         <span class="names-styles">${product.name}</span>
-                        <span><svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#CAAFA7" d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z"/></svg></i></span>
+                        <span class="removeCart-btn"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#CAAFA7" d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z"/></svg></i></span>
                     </div>
                     <div class="infoCart">
                         <span class="quantity-styles">X${product.quantity}</span>
                         <span class="price-styles">$ ${product.price.toFixed(2)}</span>
-                        <span class="price-styles">$ ${(product.price * product.quantity).toFixed(2)}</span>
+                        <span class="price-styles-total">$ ${(product.price * product.quantity).toFixed(2)}</span>
                     </div>
                 `;
                 cartContainer.append(itemElement);
@@ -79,7 +79,7 @@ export class Cart {
             const totalPrice = document.createElement("div");
             totalPrice.className = "total-price";
             totalPrice.innerHTML = `<span>Order Total</span>
-            <span id="price-total">$ ${this._total.toFixed(2)}</span>`;
+            <span id="price-total">$ ${this._total.toFixed(2)}</span>`; 
 
             const completeOrderBtn = document.createElement("div");
             completeOrderBtn.className = "order-btn";
@@ -88,13 +88,15 @@ export class Cart {
             completeOrderBtn.addEventListener("click", () => {
                 const modal = document.createElement("div");
                 modal.className = "modal";
-                modal.innerHTML = `
-                <div class="modal-content">
-                    <span class="close-btn">&times;</span>
-                    <h2>Your order has been successfully completed!</h2>
-                </div>`;
-
-            document.body.appendChild(modal);
+                if (this.total != 0){
+                    modal.innerHTML = `
+                    <div class="modal-content">
+                        <span class="close-btn">&times;</span>
+                        <h2>Your order has been successfully completed!</h2>
+                    </div>`;
+    
+                document.body.appendChild(modal);
+                }
 
             const closeBtn = modal.querySelector(".close-btn");
             if(closeBtn){
